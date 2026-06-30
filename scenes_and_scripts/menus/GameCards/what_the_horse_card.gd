aa86acbae7076d0a6bf9165b2ebd7d7bdf35c63c
TrentonJW <twesley@hmc.edu>
2026-06-29 22:48:03 -0500

    Worked on wordle card and horse card

---
+++ scenes_and_scripts/menus/GameCards/what_the_horse_card.gd
@@ -0,0 +1,14 @@
+extends Button
+
+func _ready() -> void:
+	# Connect the mouse signals to our functions
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+func _on_mouse_entered() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "rotation_degrees", 5, 0.2)
+
+func _on_mouse_exited() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "rotation_degrees", 0, 0.2)
